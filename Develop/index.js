200b1a21fdd37c16c86e29f02aa7e81ec19d89ed
Dwayne Fonville Jr <64673857+dfonville20@users.noreply.github.com>
2020-07-18 20:29:18 -0400

    Commit

---
+++ Develop/index.js
@@ -0,0 +1,12 @@
+// array of questions for user
+const fs = require("fs");
+const questions = [];
+
+// function to write README file
+function writeToFile(fileName, data) {}
+
+// function to initialize program
+function init() {}
+
+// function call to initialize program
+init();
